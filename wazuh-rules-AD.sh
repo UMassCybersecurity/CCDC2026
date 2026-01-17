2e24288e9806f13f364ffc04aec7d79f41da51a2
Michael Ye <73699238+23yem@users.noreply.github.com>
2026-01-16 22:17:36 -0500

    Enhance Active Directory detection rules and validation
    Updated Active Directory detection rules in the Wazuh configuration script. Improved descriptions and syntax validation for better clarity and functionality.

--- wazuh-rules-AD.sh
+++ wazuh-rules-AD.sh
@@ -8,13 +8,23 @@ fi
 
 RULES_FILE="/var/ossec/etc/rules/local_rules.xml"
 
-echo "Adding Active Directory detection rules to $RULES_FILE..."
+echo "Appending CCDC Active Directory group to $RULES_FILE..."
 
-# Append the rules in the specific order requested
-cat <<EOF >> $RULES_FILE
+# Append as a completely separate group block
+# Note: 'EOF' in quotes protects your $ and \ characters
+cat <<'EOF' >> $RULES_FILE
 
-<group name="security_event, windows,">
- 
+<group name="ccdc_windows_threats,">
+
+  <rule id="110009" level="0">
+    <if_sid>60103</if_sid>
+    <field name="win.system.eventID">^4662$</field>
+    <field name="win.eventdata.properties" type="pcre2">{1131f6aa-9c07-11d1-f79f-00c04fc2dcd2}|{19195a5b-6da0-11d0-afd3-00c04fd930c9}</field>
+    <field name="win.eventdata.SubjectUserName" type="pcre2">\\\$</field>
+    <options>no_full_log</options>
+    <description>Ignore machine account DCSync noise</description>
+  </rule>
+
   <rule id="110001" level="12">
     <if_sid>60103</if_sid>
     <field name="win.system.eventID">^4662$</field>
@@ -22,30 +32,21 @@ cat <<EOF >> $RULES_FILE
     <options>no_full_log</options>
     <description>Directory Service Access. Possible DCSync attack</description>
   </rule>
- 
- <rule id="110009" level="0">
-    <if_sid>60103</if_sid>
-    <field name="win.system.eventID">^4662$</field>
-    <field name="win.eventdata.properties" type="pcre2">{1131f6aa-9c07-11d1-f79f-00c04fc2dcd2}|{19195a5b-6da0-11d0-afd3-00c04fd930c9}</field>
-    <field name="win.eventdata.SubjectUserName" type="pcre2">\\$$</field>
-    <options>no_full_log</options>
-    <description>Ignore all Directory Service Access that is originated from a machine account containing $</description>
-  </rule>
- 
+
   <rule id="110002" level="12">
     <if_sid>60103</if_sid>
     <field name="win.system.eventID">^4769$</field>
     <field name="win.eventdata.TicketOptions" type="pcre2">0x40810000</field>
     <field name="win.eventdata.TicketEncryptionType" type="pcre2">0x17</field>
     <options>no_full_log</options>
-    <description>Possible Keberoasting attack</description>
+    <description>Possible Kerberoasting attack</description>
   </rule>
- 
+
   <rule id="110003" level="12">
     <if_sid>60103</if_sid>
     <field name="win.system.eventID">^4624$</field>
     <field name="win.eventdata.LogonGuid" type="pcre2">{00000000-0000-0000-0000-000000000000}</field>
-    <field name="win.eventdata.logonType" type="pcre2">3</field>
+    <field name="win.eventdata.logonType">3</field>
     <options>no_full_log</options>
     <description>Possible Golden Ticket attack</description>
   </rule>
@@ -53,9 +54,9 @@ cat <<EOF >> $RULES_FILE
   <rule id="110004" level="12">
     <if_sid>61600</if_sid>
     <field name="win.system.eventID" type="pcre2">17|18</field>
-    <field name="win.eventdata.PipeName" type="pcre2">\\\\PSEXESVC</field>
+    <field name="win.eventdata.PipeName" type="pcre2">\\PSEXESVC</field>
     <options>no_full_log</options>
-    <description>PsExec service launched for possible lateral movement within the domain</description>
+    <description>PsExec service launched for lateral movement</description>
   </rule>
 
   <rule id="110006" level="12">
@@ -68,24 +69,30 @@ cat <<EOF >> $RULES_FILE
     <if_sid>60103</if_sid>
     <field name="win.system.eventID">^4624$</field>
     <field name="win.eventdata.LogonProcessName" type="pcre2">seclogo</field>
-    <field name="win.eventdata.LogonType" type="pcre2">9</field>
+    <field name="win.eventdata.LogonType">9</field>
     <field name="win.eventdata.AuthenticationPackageName" type="pcre2">Negotiate</field>
     <field name="win.eventdata.LogonGuid" type="pcre2">{00000000-0000-0000-0000-000000000000}</field>
     <options>no_full_log</options>
     <description>Possible Pass the hash attack</description>
   </rule>
-  
+
   <rule id="110008" level="12">
     <if_sid>61612</if_sid>
-    <field name="win.eventdata.TargetImage" type="pcre2">(?i)\\\\\\\\system32\\\\\\\\lsass.exe</field>
+    <field name="win.eventdata.TargetImage" type="pcre2">(?i)\\\\lsass\.exe</field>
     <field name="win.eventdata.GrantedAccess" type="pcre2">(?i)0x1010</field>
     <description>Possible credential dumping using mimikatz</description>
   </rule>
-  
+
 </group>
 EOF
 
-echo "Restarting Wazuh Manager to apply changes..."
-systemctl restart wazuh-manager
+echo "Validating rules syntax..."
+/var/ossec/bin/wazuh-analysisd -t
 
-echo "Done!"
+if [ $? -eq 0 ]; then
+    echo "Syntax OK. Restarting Wazuh Manager..."
+    systemctl restart wazuh-manager
+    echo "Done!"
+else
+    echo "Syntax error! Use 'sudo tail -n 20 $RULES_FILE' to see what went wrong."
+fi
